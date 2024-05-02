5a20cb83dc2f29dc331099d1295d54a9ea4e176d
flobo <flo6079@hs-el.de>
2024-05-02 15:07:53 +0200

    servlet details done

--- src/main/java/servlets/BookDetails.java
+++ src/main/java/servlets/BookDetails.java
@@ -7,6 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import database.DatabaseStatements;
+import objects.Buch;
+import thymeleaf.ThymeleafConfig;
+
 /**
  * Servlet implementation class BookDetails
  */
@@ -34,7 +41,20 @@ public class BookDetails extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		TemplateEngine engine = ThymeleafConfig.getTemplateEngine();
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        response.setCharacterEncoding("UTF-8");
+        
+        DatabaseStatements dbstatements = new DatabaseStatements();
+        String isbn = request.getParameter("isbn");
+        Buch buch = dbstatements.getBook(isbn);
+        context.setVariable("titel", buch.getTitel());
+        context.setVariable("autor", buch.getAutor());
+        context.setVariable("preis", buch.getPreis());
+        context.setVariable("kategorien", buch.getKategorien());
+        context.setVariable("isbn", buch.getIsbn());
+        context.setVariable("beschreibung", buch.getBeschreibung());
+        engine.process("bookdetails.html", context, response.getWriter());
 	}
 
 }
