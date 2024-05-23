826735fc42cac0b5dbbbd4c949d8ceb827d296ff
flobo <flo6079@hs-el.de>
2024-05-23 14:31:30 +0200

    awdiha

--- src/main/java/JSF/ShoppingcartHandler.java
+++ src/main/java/JSF/ShoppingcartHandler.java
@@ -1,6 +1,8 @@
 package JSF;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,10 +35,10 @@ public class ShoppingcartHandler implements Serializable{
 			total += b.getPreis().doubleValue();
 			Item i = new Item(b, 1);
 			book = i;
+			System.out.println(book.getBuch().getTitel());
 			books.add(i);
 		}
 	}
-	
 	
 	public List<Item> getBooks() {
 		return books;
@@ -55,7 +57,7 @@ public class ShoppingcartHandler implements Serializable{
 		Buch buch = dbstatements.getBook(isbn);
 		Item item = new Item(buch , 1);
 		numberItems += 1;
-		total += buch.getPreis().doubleValue();
+		addTotal(buch.getPreis().doubleValue());
 		books.add(item);
 	}
 	
@@ -64,21 +66,21 @@ public class ShoppingcartHandler implements Serializable{
 		double amount = 0;
 		Item remove = null;
 		for (Item i : books) {
-			if(i.getBuch().equals(item)) {
+			if(i.equals(item)) {
 				remove = i;
 				price = i.getAnzahl() * i.getBuch().getPreis().doubleValue();
 				amount = i.getAnzahl();
 				break;
 			}
 		}
-		total -= price;
+		subTotal(price);
 		numberItems -= amount;
 		books.remove(remove);
 	}
 
 	public void addAmount(Item item) {
 		numberItems++;
-		total += book.getBuch().getPreis().doubleValue();
+		addTotal(item.getBuch().getPreis().doubleValue());
 		DataHelper hlp = new DataHelper();
 		books.get(hlp.findVal(books, item)).setAnzahl(item.getAnzahl() + 1);
 		
@@ -89,10 +91,26 @@ public class ShoppingcartHandler implements Serializable{
 			return;
 		}
 		numberItems--;
-		total -= book.getBuch().getPreis().doubleValue();
+		subTotal(item.getBuch().getPreis().doubleValue());
 		DataHelper hlp = new DataHelper();
 		books.get(hlp.findVal(books, item)).setAnzahl(item.getAnzahl() - 1);
 	}
 	
+	private double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+	    BigDecimal bd = BigDecimal.valueOf(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+	
+	private void addTotal(double price) {
+		total += price;
+		total = round(total, 2);
+	}
+	
+	private void subTotal(double price) {
+		total -= price;
+		total = round(total, 2);	
+	}
 
 }
